a3a08731d355c7a7ad3eb8c8f9868da123b00963
Anthony Utt <anthonyutt@hotmail.com>
2020-06-25 20:45:47 -0400

    TravisCI setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+test('renders', () => {
+  const { getAllByText } = render(<App />);
+  const linkElement = getAllByText(/sign in/i)[0];
   expect(linkElement).toBeInTheDocument();
 });
